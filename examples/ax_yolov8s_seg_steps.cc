35faeea0c3a6b53bac71b046062d7105cb531d16
FeiGeChuanShu <774074168@qq.com>
2023-01-15 18:27:05 +0800

    update yolov8 with argmax (#66)
    * update yolov8 with argmax
    
    * Update CMakeLists.txt

--- examples/ax_yolov8s_seg_steps.cc
+++ examples/ax_yolov8s_seg_steps.cc
@@ -40,7 +40,8 @@
 
 const int DEFAULT_IMG_H = 640;
 const int DEFAULT_IMG_W = 640;
-
+const int DEFAULT_MASK_PROTO_DIM = 32;
+const int DEFAULT_MASK_SAMPLE_STRIDE = 4;
 const char* CLASS_NAMES[] = {
     "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
     "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
@@ -51,7 +52,21 @@ const char* CLASS_NAMES[] = {
     "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
     "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
     "hair drier", "toothbrush"};
-
+static const std::vector<std::vector<uint8_t> > COCO_COLORS= {
+    {56,  0,   255}, {226, 255, 0}, {0,   94,  255}, {0,   37,  255}, {0,   255, 94}, {255, 226, 0},
+    {0,   18,  255}, {255, 151, 0}, {170, 0,   255}, {0,   255, 56}, {255, 0,   75}, {0,   75,  255},
+    {0,   255, 169}, {255, 0,   207}, {75,  255, 0}, {207, 0,   255}, {37,  0,   255}, {0,   207, 255},
+    {94,  0,   255}, {0,   255, 113}, {255, 18,  0}, {255, 0,   56}, {18,  0,   255}, {0,   255, 226},
+    {170, 255, 0}, {255, 0,   245}, {151, 255, 0}, {132, 255, 0}, {75,  0,   255}, {151, 0,   255},
+    {0,   151, 255}, {132, 0,   255}, {0,   255, 245}, {255, 132, 0}, {226, 0,   255}, {255, 37,  0},
+    {207, 255, 0}, {0,   255, 207}, {94,  255, 0}, {0,   226, 255}, {56,  255, 0}, {255, 94,  0},
+    {255, 113, 0}, {0,   132, 255}, {255, 0,   132}, {255, 170, 0}, {255, 0,   188}, {113, 255, 0},
+    {245, 0,   255}, {113, 0,   255}, {255, 188, 0}, {0,   113, 255}, {255, 0,   0}, {0,   56,  255},
+    {255, 0,   113}, {0,   255, 188}, {255, 0,   94}, {255, 0,   18}, {18,  255, 0}, {0,   255, 132},
+    {0,   188, 255}, {0,   245, 255}, {0,   169, 255}, {37,  255, 0}, {255, 0,   151}, {188, 0,   255},
+    {0,   255, 37}, {0,   255, 0}, {255, 0,   170}, {255, 0,   37}, {255, 75,  0}, {0,   0,   255},
+    {255, 207, 0}, {255, 0,   226}, {255, 245, 0}, {188, 255, 0}, {0,   255, 18}, {0,   255, 75},
+    {0,   255, 151}, {255, 56,  0}, {245, 255, 0}};
 const int DEFAULT_LOOP_COUNT = 1;
 const int CLS_NUM = 80;
 const float PROB_THRESHOLD = 0.3f;
@@ -252,22 +267,25 @@ namespace ax
         fprintf(stdout, "run over: output len %d\n", io_info->nOutputSize);
 
         // 5. get bbox
-        std::vector<det::Object> proporsel;
+        std::vector<det::Object> proposals;
         std::vector<det::Object> objects;
 
         for (uint32_t i = 0; i < 3; ++i)
         {
-            auto& dfl_info = joint_io_arr.pOutputs[i];
-            auto dfl_ptr = (float*)dfl_info.pVirAddr;
             auto& cls_info = joint_io_arr.pOutputs[i + 3];
             auto cls_ptr = (float*)cls_info.pVirAddr;
             auto& cls_idx_info = joint_io_arr.pOutputs[i + 6];
             auto cls_idx_ptr = (float*)cls_idx_info.pVirAddr;
+            auto& dfl_info = joint_io_arr.pOutputs[i];
+            auto dfl_ptr = (float*)dfl_info.pVirAddr;
             int32_t stride = (1 << i) * 8;
-            det::generate_proposals_yolov8(stride, dfl_ptr, cls_ptr, cls_idx_ptr, PROB_THRESHOLD, proporsel, input_w, input_h, CLS_NUM);
+            det::generate_proposals_yolov8_seg(stride, dfl_ptr, cls_ptr, cls_idx_ptr, PROB_THRESHOLD, proposals, input_w, input_h, CLS_NUM);
         }
 
-        det::get_out_bbox(proporsel, objects, NMS_THRESHOLD, input_h, input_w, mat.rows, mat.cols);
+        auto& info = joint_io_arr.pOutputs[9];
+        auto ptr = (float*)info.pVirAddr;
+        det::get_out_bbox_mask(proposals, objects, ptr, DEFAULT_MASK_PROTO_DIM, DEFAULT_MASK_SAMPLE_STRIDE, NMS_THRESHOLD, input_h, input_w, mat.rows, mat.cols);
+
         // 6. show time costs
         fprintf(stdout, "--------------------------------------\n");
         fprintf(stdout,
@@ -290,7 +308,7 @@ namespace ax
         fprintf(stdout, "--------------------------------------\n");
         fprintf(stdout, "detection num: %d\n", objects.size());
 
-        det::draw_objects(mat, objects, CLASS_NAMES, "yolov8s");
+        det::draw_objects_mask(mat, objects, CLASS_NAMES, COCO_COLORS, "yolov8s_seg");
         clear_and_exit();
         return true;
     }
